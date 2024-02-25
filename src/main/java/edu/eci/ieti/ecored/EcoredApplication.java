e3ef98c57cf1b161fd41ac8173eeb01305ae087a
Daniel Santanilla <daniel1sa_@hotmail.com>
2024-02-24 20:22:43 -0500

    Initial files

---
+++ src/main/java/edu/eci/ieti/ecored/EcoredApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.ecored;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoredApplication.class, args);
+	}
+
+}
