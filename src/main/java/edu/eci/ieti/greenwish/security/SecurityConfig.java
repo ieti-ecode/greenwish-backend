6e20da7febd0a13f83082e39edebbeb0a01e181c
Angie Mojica <nataliamojica90@gmail.com>
2024-04-15 23:51:16 -0500

    remove issue

--- src/main/java/edu/eci/ieti/greenwish/security/SecurityConfig.java
+++ src/main/java/edu/eci/ieti/greenwish/security/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
