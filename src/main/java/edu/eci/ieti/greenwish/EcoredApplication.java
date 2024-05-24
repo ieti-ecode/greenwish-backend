89ad8ce6e9151be5936e1166148961c2d9bbab7e
Daniel Santanilla <daniel1sa_@hotmail.com>
2024-05-24 17:59:48 -0500

    Admin de prueba

--- src/main/java/edu/eci/ieti/greenwish/EcoredApplication.java
+++ src/main/java/edu/eci/ieti/greenwish/EcoredApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import edu.eci.ieti.greenwish.models.dto.UserDto;
+import edu.eci.ieti.greenwish.models.domain.Role;
+import edu.eci.ieti.greenwish.models.domain.User;
+import edu.eci.ieti.greenwish.repositories.UserRepository;
 import edu.eci.ieti.greenwish.services.UserService;
 
 @SpringBootApplication
@@ -14,6 +17,12 @@ public class EcoredApplication {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public static void main(String[] args) {
         SpringApplication.run(EcoredApplication.class, args);
@@ -23,12 +32,13 @@ public class EcoredApplication {
     CommandLineRunner initDatabase() {
         return args -> {
             if (!userService.existsAdminUser()) {
-                UserDto adminDto = new UserDto();
-                adminDto.setName("GreenWish");
-                adminDto.setEmail("admin@greenwish.com");
-                adminDto.setPassword("adminpassword");
-                adminDto.setIsCompany(false);
-                userService.save(adminDto);
+                User admin = new User();
+                admin.setName("admin");
+                admin.setEmail("greenwish@ecode.com");
+                admin.setPasswordHash(passwordEncoder.encode("admin"));
+                admin.setRole(Role.ADMINISTRATOR.getName());
+                admin.setPoints(10000);
+                userRepository.save(admin);
             }
         };
     }
